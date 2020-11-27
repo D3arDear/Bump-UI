6a2b1b8a06a77fccb2740280166dff965b5124ed
Brendan <ex.arc.zhang@gmail.com>
2020-11-27 16:32:48 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hi</div>
+  <div>Welcome to Bump UI</div>
+  <Test />
+</template>
+
+<script>
+import Test from "./components/test.vue";
+
+export default {
+  name: "App",
+  components: {
+    Test,
+  },
+};
+</script>

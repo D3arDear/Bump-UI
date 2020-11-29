958323fa2968c8335e1582d8e9489ec7e7a4860f
Brendan <ex.arc.zhang@gmail.com>
2020-11-29 09:35:17 +0800

    complete aside toggleable

--- src/components/views/Doc.vue
+++ src/components/views/Doc.vue
@@ -2,7 +2,7 @@
   <div>
     <Topnav></Topnav>
     <div class="content">
-      <aside>
+      <aside v-if="asideVisible">
         <ul>
           <li>Switch 组件</li>
           <li>Button 组件</li>
@@ -10,16 +10,22 @@
           <li>Tabs 组件</li>
         </ul>
       </aside>
-      <main>主内容</main>
+      <main :class="{ asideVisible: asideVisible }">主内容</main>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { inject, Ref } from "vue";
 import Topnav from "./common/Topnav.vue";
 
 export default {
   components: { Topnav },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("xxx");
+    console.log("doc 获取到 asideVisible 为: " + asideVisible.value);
+    return { asideVisible };
+  },
   name: "Doc",
 };
 </script>
@@ -28,7 +34,6 @@ export default {
 .content {
   aside {
     position: absolute;
-    margin-top: 50px;
     top: 0;
     left: 0;
     bottom: 0;
@@ -64,6 +69,9 @@ export default {
         box-shadow: -2px -2px 4px rgba($color: #ffffff, $alpha: 0.5), 2px 2px 4px rgba($color: #000000, $alpha: 0.2);
       }
     }
+  }
+  main.asideVisible {
+    margin-left: 208px;
   }
 }
 </style>
--- src/components/views/common/Topnav.vue
+++ src/components/views/common/Topnav.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="Topnav">
-    <div class="logo">LOGO</div>
+  <header class="Topnav" :class="{ asideVisible: asideVisible }">
+    <div class="logo" @click="toggleMenu">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -9,12 +9,26 @@
 </template>
 
 <script lang="ts">
+import { inject, Ref } from "vue";
 export default {
   name: "Topnav",
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("xxx");
+    console.log("topnav 获取到 asideVisible 为: " + asideVisible.value);
+    const toggleMenu = () => {
+      console.log("点击了");
+      asideVisible.value = !asideVisible.value;
+      console.log(asideVisible.value);
+    };
+    return { toggleMenu, asideVisible };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.Topnav.asideVisible {
+  padding-left: 208px;
+}
 .Topnav {
   display: flex;
   justify-content: space-between;

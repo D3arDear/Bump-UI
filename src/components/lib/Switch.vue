101c7e15ba973beb18f30bec0271bb838ee1fc54
Brendan <ex.arc.zhang@gmail.com>
2020-12-10 16:21:29 +0800

    checkedable switch

--- src/components/lib/Switch.vue
+++ src/components/lib/Switch.vue
@@ -1,10 +1,18 @@
 <template>
-  <button><span></span></button>
+  <button @click="toggle" :class="{ checked }"><span></span></button>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 export default {
   name: "Switch",
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  },
 };
 </script>
 
@@ -42,14 +50,17 @@ $tiny-shadow: -2px -2px 4px rgba(
       $color: #000000,
       $alpha: 0.4,
     );
+
 button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: linear-gradient(145deg, #e06a2a, #ff7e32);
+  /* background: linear-gradient(145deg, #e06a2a, #ff7e32); */
+  background: #ccc;
   border-radius: $h/2;
   position: relative;
   box-shadow: $small-shadow, $small-shadow-inset;
+  transition: background 250ms;
 }
 span {
   position: absolute;
@@ -60,8 +71,15 @@ span {
   background: linear-gradient(145deg, #ffffff, #dfdfdf);
   border-radius: $h2/2;
   box-shadow: $tiny-shadow;
+  transition: left 250ms;
 }
-button:hover > span {
+button.checked {
+  background: linear-gradient(145deg, #e06a2a, #ff7e32);
+}
+button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
 }
 </style>

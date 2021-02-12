0a2b94844ce94b6647e2cd55a43ea7e531a01e86
Brendan <ex.arc.zhang@gmail.com>
2021-02-12 17:23:56 +0800

    switch B.E.M classes

--- src/components/lib/Switch/Switch.normal.vue
+++ src/components/lib/Switch/Switch.normal.vue
@@ -1,11 +1,11 @@
 <template>
-  <button :class="{ checked: value, flat: flat }">
+  <button :class="[classes('', '', flat ? 'flat' : ''), { checked: value }]">
     <span></span>
   </button>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
+import { classMaker } from "../common/classMaker";
 export default {
   name: "SwitchNormal",
   props: {
@@ -16,100 +16,12 @@ export default {
     },
   },
   setup(props, context) {
-    return {};
+    const classes = classMaker("BUI-Switch");
+    return { classes };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../style/theme.scss";
-$h: 22px;
-$h2: $h - 4px;
-$small-shadow: 2px 2px 6px
-    rgba(
-      $color: #000,
-      $alpha: 0.3,
-    ),
-  -2px -2px 6px
-    rgba(
-      $color: #fff,
-      $alpha: 0.7,
-    );
-
-$small-shadow-inset: 2px 2px 5px inset
-    rgba(
-      $color: #000,
-      $alpha: 0.3,
-    ),
-  -2px -2px 5px inset
-    rgba(
-      $color: #fff,
-      $alpha: 0.5,
-    );
-
-$tiny-shadow: -2px -2px 4px rgba(
-      $color: #ffffff,
-      $alpha: 0.6,
-    ),
-  2px 2px 4px
-    rgba(
-      $color: #000000,
-      $alpha: 0.4,
-    );
-
-button {
-  position: relative;
-  height: $h;
-  padding: 0;
-  width: $h * 2;
-  border: none;
-  display: inline-flex;
-  border-radius: $h/2;
-  position: relative;
-  z-index: -1;
-  box-shadow: shadow-generator(topLeft, #f8f8f8, 6px, true),
-    shadow-generator(topLeft, #f8f8f8, 6px, false);
-  transition: background 250ms;
-  @include surface(topLeft, #f8f8f8, concave);
-  &:after {
-    content: "";
-    border-radius: $h/2;
-    z-index: 0;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    opacity: 0;
-    transition: opacity 250ms;
-    @include shadow(topLeft, #ff9e5d, 4px, true);
-    @include surface(topLeft, #ff9e5d, concave);
-  }
-  &.checked {
-    &:after {
-      opacity: 1;
-    }
-    > span {
-      left: calc(100% - #{$h2} - 2px);
-    }
-  }
-  &.flat {
-    @include shadow(topLeft, #f8f8f8, 4px, true);
-  }
-  &:focus {
-    outline: none;
-  }
-  > span {
-    position: absolute;
-    z-index: 1;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: linear-gradient(145deg, #ffffff, #dfdfdf);
-    border-radius: $h2/2;
-    box-shadow: $tiny-shadow;
-    transition: left 250ms;
-  }
-}
+@import "./Switch.normal.scss";
 </style>
--- src/components/lib/Switch/Switch.retro.vue
+++ src/components/lib/Switch/Switch.retro.vue
@@ -1,124 +1,28 @@
 <template>
-  <button class="SwitchCover" :class="{ checked: value }">
-    <div class="Switch">
-      <div class="SwitchHandle"></div>
+  <button :class="[classes('Retro', '', ''), { checked: value }]">
+    <div :class="[classes('Retro', 'handle', '')]">
+      <div :class="[classes('Retro', 'handle-item', '')]"></div>
     </div>
-    <div class="SwitchShadow-cover">
-      <div class="SwitchShadow"></div>
+    <div :class="classes('Retro', 'shadow-cover', '')">
+      <div :class="classes('Retro', 'shadow', '')"></div>
     </div>
   </button>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
+import { classMaker } from "../common/classMaker";
 export default {
   name: "SwitchRetro",
   props: {
     value: Boolean,
   },
   setup(props, context) {
-    return {};
+    const classes = classMaker("BUI-Switch");
+    return { classes };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../style/theme.scss";
-$h: 22px;
-$gap: 2px;
-$h2: $h - 2 * $gap;
-
-.SwitchCover {
-  position: relative;
-  min-width: $h * 2;
-  min-height: $h;
-  padding: $gap;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  background: #f8f8f8;
-  padding: 0;
-  z-index: -1;
-  border-radius: $h/4;
-  border: 0;
-  outline: 0px;
-  cursor: pointer;
-  @include surface(topLeft, #f8f8f8, convex);
-  @include shadow(topLeft, #f8f8f8, 6px, false);
-  transition: all 250ms;
-  cursor: pointer;
-  &:after {
-    content: "";
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    border-radius: $h/4;
-    @include surface(topLeft, #ff9e5d, convex);
-    transition: all 250ms;
-  }
-  .Switch {
-    position: absolute;
-    z-index: 1;
-    inset: $gap;
-    width: 2 * ($h2 + $gap);
-    outline: none;
-    border: none;
-    @include surface(topRight, #fff, convex);
-    border-radius: $h2/4;
-    padding: 0;
-    transition: all 250ms;
-    &:after {
-      content: "";
-      position: absolute;
-      opacity: 0;
-      inset: 0;
-      width: 2 * ($h2 + $gap);
-      border-radius: $h2/4;
-      @include surface(bottomRight, #ddd, convex);
-      transition: all 250ms;
-    }
-    .SwitchHandle {
-      position: absolute;
-      top: $h2/2 - $h2/4;
-      right: 2 * $gap/3;
-      height: $h2/2;
-      width: $gap/2;
-      border-radius: $gap/2;
-      background: #ddd;
-      transition: all 250ms;
-    }
-  }
-  &.checked {
-    &:after {
-      opacity: 1;
-    }
-    .Switch {
-      &:after {
-        opacity: 1;
-      }
-      .SwitchHandle {
-        z-index: 2;
-        background: #ff9e5d;
-      }
-    }
-  }
-  .SwitchShadow-cover {
-    position: absolute;
-    inset: $gap;
-    z-index: -1;
-    filter: drop-shadow(4px 4px 3px rgba($color: #000000, $alpha: 0.2))
-      drop-shadow(-2px -4px 3px rgba($color: rgb(255, 255, 255), $alpha: 0.8));
-    .SwitchShadow {
-      z-index: -1;
-      position: absolute;
-      inset: $gap;
-      clip-path: polygon(10% 20%, 100% 0, 100% 100%, 10% 80%);
-      z-index: 2;
-      background-color: rgba(255, 255, 255, 1);
-    }
-  }
-}
+@import "./Switch.retro.scss";
 </style>

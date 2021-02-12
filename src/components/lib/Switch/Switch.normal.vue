b3a3f7ef23c9544aaae24e8abd97497deb340e69
Brendan <ex.arc.zhang@gmail.com>
2021-02-13 00:33:35 +0800

    fix a bug of Switch flat

--- src/components/lib/Switch/Switch.normal.vue
+++ src/components/lib/Switch/Switch.normal.vue
@@ -22,6 +22,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "./Switch.normal.scss";
 </style>
--- src/components/lib/Switch/Switch.retro.vue
+++ src/components/lib/Switch/Switch.retro.vue
@@ -23,6 +23,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "./Switch.retro.scss";
 </style>

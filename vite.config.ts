3e473bbf4c199f79b6905eefe6a0055944b7905b
Brendan <ex.arc.zhang@gmail.com>
2021-03-12 11:14:56 +0800

    adjust README

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { baseParse } from "@vue/compiler-core";
 
 export default {
   base: "/",
-  assetsDir: "./src/assets",
+  assetsDir: "assets",
   plugins: [md()],
   vueCustomBlockTransforms: {
     demo: (options) => {

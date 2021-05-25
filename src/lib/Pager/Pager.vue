20cf793a17cd232eb585f9435b86f5aa63eb3aa8
Brendan <ex.arc.zhang@gmail.com>
2021-05-25 12:31:28 +0800

    fix a bug of pager

--- src/lib/Pager/Pager.vue
+++ src/lib/Pager/Pager.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="BUI-pager"
-    :class="{ hide: hideIfOnePage === true && totalPage <= 1 }"
-  >
+  <div class="BUI-pager" :class="{ hide: hideIfOnePage === true && totalPage <= 1 }">
     <span
       class="BUI-pager-nav prev"
       :class="{ disabled: currentPage === 1 }"
@@ -20,9 +17,7 @@
         <Icon class="BUI-pager-separator" name="dots"></Icon>
       </template>
       <template v-else>
-        <span class="BUI-pager-item other" @click="onClickPage(page)">{{
-          page
-        }}</span>
+        <span class="BUI-pager-item other" @click="onClickPage(page)">{{ page }}</span>
       </template>
     </template>
     <span
@@ -35,17 +30,16 @@
   </div>
 </template>
 
-
 <script lang="ts">
-import { computed } from 'vue';
+import { computed } from "vue";
 const unique = (array) => {
   //  return [...new Set(array)];
   const object = {};
-  array.map(number => {
+  array.map((number) => {
     object[number] = true;
   });
-  return Object.keys(object).map(s => parseInt(s, 10));
-}
+  return Object.keys(object).map((s) => parseInt(s, 10));
+};
 import Icon from "../Icon.vue";
 
 export default {
@@ -54,50 +48,80 @@ export default {
   props: {
     totalPage: {
       type: Number,
-      required: true
+      required: true,
     },
     currentPage: {
       type: Number,
-      required: true
+      required: true,
     },
     hideIfOnePage: {
       type: Boolean,
-      default: true
-    }
+      default: true,
+    },
   },
   setup(props, context) {
     const pages = computed(() => {
       return unique(
-        [
-          1,
-          props.totalPage,
-          props.currentPage,
-          props.currentPage - 1,
-          props.currentPage - 2,
-          props.currentPage + 1,
-          props.currentPage + 2
-        ]
-          .filter(n => n >= 1 && n <= props.totalPage)
-          .sort((a, b) => a - b)
+        props.currentPage <= 4
+          ? props.totalPage <= 7
+            ? [1, props.currentPage, props.totalPage]
+            : [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                props.totalPage,
+                props.currentPage,
+                props.currentPage + 1,
+                props.currentPage + 2,
+              ]
+          : props.currentPage >= props.totalPage - 6
+          ? [
+              1,
+              props.totalPage - 1,
+              props.totalPage - 2,
+              props.totalPage - 3,
+              props.totalPage - 4,
+              props.totalPage - 5,
+              props.totalPage - 6,
+              props.currentPage,
+              props.totalPage,
+            ]
+          : [
+              1,
+              props.totalPage,
+              props.currentPage,
+              props.currentPage - 1,
+              props.currentPage + 1,
+              props.currentPage - 2,
+              props.currentPage + 2,
+            ]
+              .filter((n) => n >= 1 && n <= props.totalPage)
+              .sort((a, b) => a - b)
       ).reduce((prev, current, index, array) => {
-        prev.push(current); // 1
+        prev.push(current);
         array[index + 1] !== undefined &&
           array[index + 1] - array[index] > 1 &&
           prev.push("...");
+        // array[index + 1] !== undefined &&
+        //   array[index + 1] - array[index] > 1 &&
+        //   prev.push("...");
         return prev;
       }, []);
-    })
+    });
 
     const onClickPage = (n) => {
       if (n >= 1 && n <= props.totalPage) {
         context.emit("update:currentPage", n);
       }
-    }
+    };
     return {
       pages,
-      onClickPage
-    }
-  }
+      onClickPage,
+    };
+  },
 };
 </script>
-
